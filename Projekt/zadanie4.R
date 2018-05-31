7bd3b45c569ef80835a9850633143f6fb2c90d16
Michal <maq0297@gmail.com>
2018-05-31 16:37:02 +0200

    3 i 4 - robota Michala

--- Projekt/zadanie4.R
+++ Projekt/zadanie4.R
@@ -1,11 +1,14 @@
 dane=unname(unlist(read.table("nowa_hala.txt")))  
 srednia = mean(dane)
-wariancja = var(dane)
+srednia
 odchylenie_standardowe = sd(dane)
+odchylenie_standardowe
 kwantyl_rozkladu_normalnego = 1.96 #dla alpha = 0,05 , z jakiejs tabelki
 n = length(dane)
 poczatek_przedzialu = odchylenie_standardowe/(1+ kwantyl_rozkladu_normalnego/sqrt(2*n)) 
 koniec_przedzialu = odchylenie_standardowe/(1 - kwantyl_rozkladu_normalnego/sqrt(2*n)) 
+poczatek_przedzialu
+koniec_przedzialu
 d = 0.5*(koniec_przedzialu - poczatek_przedzialu)
 d
 wzgledna_precyzja_oszaczowania = (d/odchylenie_standardowe) * 100
