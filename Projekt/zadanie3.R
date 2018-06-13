57f6bec68f4d4f86ab89d7337e6f8d1b02c9a284
DESKTOP-D351Q1F\Bartek <bac2@vp.pl>
2018-06-13 22:41:07 +0200

    zmiany estetyczne zad3, zad4

--- Projekt/zadanie3.R
+++ Projekt/zadanie3.R
@@ -1,20 +1,12 @@
 dane=unname(unlist(read.table("stara_hala.txt")))  
 srednia = mean(dane)
-srednia
 odchylenie_standardowe = sd(dane)
-odchylenie_standardowe
-kwantyl_rozkladu_normalnego = 1.96 #dla alpha = 0,05 , z jakiejs tabelki
+kwantyl_rozkladu_normalnego = qnorm(0.975) #dla alfa=0.05
 n = length(dane)
 poczatek_przedzialu = srednia - kwantyl_rozkladu_normalnego*odchylenie_standardowe/sqrt(n) 
 koniec_przedzialu = srednia + kwantyl_rozkladu_normalnego*odchylenie_standardowe/sqrt(n)
 poczatek_przedzialu
 koniec_przedzialu
-d=srednia- poczatek_przedzialu
-d
-d2 =koniec_przedzialu-srednia
-d2
-d3 = kwantyl_rozkladu_normalnego*odchylenie_standardowe/sqrt(n)
-d3
+d=srednia - poczatek_przedzialu
 wzgledna_precyzja_oszaczowania = (d/srednia) * 100
 wzgledna_precyzja_oszaczowania
-help(rug)
