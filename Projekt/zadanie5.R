53eb3a2c5544037d7b1b4009b90bc09028077aea
martyna8821 <martyna@email.com>
2018-06-14 13:23:21 +0200

    koniec

--- Projekt/zadanie5.R
+++ Projekt/zadanie5.R
@@ -6,13 +6,13 @@ n1=length(dane_nowa_hala)
 n2=length((dane_stara_hala))
 wartosc_testu_fishera=w1/w2
 wartosc_testu_fishera
-wartosc_krytyczna_rozkladu_fishera=1.761
+wartosc_krytyczna_rozkladu_fishera=qf(0.95, length(dane_nowa_hala)-1, length(dane_stara_hala)-1)
 wartosc_krytyczna_rozkladu_fishera
 z1=w1/(n1-1)
 z2=w2/(n2-1)
 c=(abs(mean(dane_stara_hala)-mean(dane_nowa_hala)))/sqrt(z2+z1)
-t1=2.0106
-t2=2.0322
+t1=qt(0.95, length(dane_nowa_hala)-1)
+t2=qt(0.95, length(dane_stara_hala)-1)
 wartosc_krytyczna_c=(z1*t1+z2*t2)/(z1+z2)
 
 c
